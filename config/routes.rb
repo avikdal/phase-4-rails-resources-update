84f4e86db45a81e47f8824da2604a13552199160
amanda vikdal <vikdal.amanda@gmail.com>
2023-10-09 12:26:38 -0400

    commit tree d57e1c9ad9f8bc9c26bea6d03222e850583d0f47

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create, :update]
+
+  #To take the responsibility for handling likes off of the frontend, we can provide a custom route that will do the work of calculating the number of likes and incrementing it. With this approach, all the frontend has to do is send a request to our new custom route, without worrying about sending any data in the body of the request.
+
+  patch "/birds/:id/like", to: "birds#increment_likes"
+
 end
